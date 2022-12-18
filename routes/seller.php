6c76feab269560cb787ca480b72beb3d7ff1ab41
DeveloperKareemElsharkawy <dev.kareem.elsharkawy@gmail.com>
2022-12-18 14:59:09 +0200

    fix Favorite Products

--- routes/seller.php
+++ routes/seller.php
@@ -537,6 +537,6 @@ Route::get('/test', function () {
     }
 });
 Route::post('pull', function () {
-    $output = shell_exec('git pull');
+    echo  $output = shell_exec('git pull');
     return  $output . ' Git Command Response' ;
 });
