3c7289ab423948ace47361b5c89d3237952bb2a2
DeveloperKareemElsharkawy <dev.kareem.elsharkawy@gmail.com>
2022-12-19 23:17:49 +0200

    fix migration

--- database/migrations/2022_12_19_230456_add_youtube_link_column_to_products_table.php
+++ database/migrations/2022_12_19_230456_add_youtube_link_column_to_products_table.php
@@ -14,7 +14,7 @@ class AddYoutubeLinkColumnToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('youtube_link')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddYoutubeLinkColumnToProductsTable extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropColumn('youtube_link');
         });
     }
 }
