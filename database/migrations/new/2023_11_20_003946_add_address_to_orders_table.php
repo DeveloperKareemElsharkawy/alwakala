6b7c392c436ff7f0ab566245164a18f8aaf8be51
Abd El-Rahman Nabil <dobanabil40@gmail.com>
2023-11-20 00:40:52 +0200

    address

--- database/migrations/new/2023_11_20_003946_add_address_to_orders_table.php
+++ database/migrations/new/2023_11_20_003946_add_address_to_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddParentIdToOrdersTable extends Migration
+class AddAddressToOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,7 @@ class AddParentIdToOrdersTable extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->integer('parent_order_id')->nullable();
-            $table->integer('store_id')->nullable();
-            $table->integer('order_price')->nullable();
-            $table->integer('shopping_cart_id')->nullable();
+            $table->text('address')->nullable();
         });
     }
 
